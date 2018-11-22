44964fc0a94f8eeae69c69d8d992a94f1b74b14c
Matthias Rudin <matthias.rudin@live.com>
2018-11-22 11:31:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
   if array.length === 0
     puts "The line is currently empty."
   else
-    array_names = array.map{|element,index| "#{index+1}. #{element}"}
+    array_names = array.map{|element,index| "#{index}. #{element}"}
     puts "The line is currently: #{array_names.to_s}"
   end
 end
